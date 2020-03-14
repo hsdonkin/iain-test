2697c9991e1d7239f5b68da827ee5b24e0390ecf
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2020-03-14 13:49:49 -0700

    Add comments for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+// React library
+// react parses HTML like <div> and lets you write it in a javascript file
 import React from 'react';
 
+// components are react objects
+// think of them like custom HTML elements
 import WhiteKey from './components/WhiteKey';
 import BlackKey from './components/BlackKey';
 
+// idfk anything about music
 const notes = [
   'c3',
   'c-3',
   'd3',
   'd-3',
   'e3',
-
   'f3',
   'f-3',
   'g3',
@@ -20,16 +24,23 @@ const notes = [
   'c4'
 ];
 
+// cycle through all of these notes, and for each one
 const PianoKeys = notes.map((note) => {
+  // if it's a sharp, create a black key
   if (note.match('-3')) {
     return <BlackKey note={note} />;
+    // if it's not, create a white key
   } else if (note.match('3')) {
     return <WhiteKey note={note} />;
+    // top of the next octave
+  } else if (note.match('-4')) {
+    return <BlackKey note={note} />;
   } else if (note.match('4')) {
     return <WhiteKey note={note} />;
   }
 });
 
+// the App is actually a component we're exporting and using in another file
 const App = () => <div className="container">{PianoKeys}</div>;
 
 export default App;
--- src/components/WhiteKey.js
+++ src/components/WhiteKey.js
@@ -1,6 +1,10 @@
+// React library
+// react parses HTML like <div> and lets you write it in a javascript file
 import React from 'react';
+// this is the function that actually plays the note
 import { playNote } from '../modules';
 
+// onClick is a user action called an Event
 const WhiteKey = ({ note }) => {
   return <div className="white-key" onClick={() => playNote(note)}></div>;
 };
